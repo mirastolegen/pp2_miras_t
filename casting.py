69882970d007a91d4e2b45ae59925b3db903051f
miras tolegen <mi_tolegen@kbtu.kz>
2026-01-27 02:57:20 +0500

    My first Code Upload

---
+++ casting.py
@@ -0,0 +1,12 @@
+x = int(2)   # x will be 2
+y = int(4.7) # y will be 4
+z = int("9") # z will be 9
+
+x = float(5)     # x will be 5.0
+y = float(6.6)   # y will be 6.6
+z = float("3")   # z will be 3.0
+w = float("4.2") # w will be 4.2
+
+x = str("t5") # x will be 't5'
+y = str(4)    # y will be '4'
+z = str(3.0)  # z will be '3.0'
