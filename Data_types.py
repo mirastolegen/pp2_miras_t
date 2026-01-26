974ef3804cf8f11bf0963eef2018b819f9c66499
mirastolegen <mirastolegen52@gmail.com>
2026-01-27 03:13:56 +0500

    Add files via upload

---
+++ Data_types.py
@@ -0,0 +1,15 @@
+x = 6 #example 1
+print (x)
+print(type(x))
+a =True#example 2
+print (a)
+print(type(a))
+b= [1,4,5,8]#example 3
+print (b)
+print(type(b))
+c=None#example 4
+print (c)
+print(type(c))
+d=range(9)#example 5
+print (d)
+print(type(d))
