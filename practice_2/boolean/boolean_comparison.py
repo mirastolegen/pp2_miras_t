853598b264fc771c6340fb5985d079b5282c8723
mirastolegen <mirastolegen52@gmail.com>
2026-02-10 00:35:51 +0500

    Add files via upload

---
+++ practice_2/boolean/boolean_comparison.py
@@ -0,0 +1,35 @@
+#1
+#==	Equal	x == y	
+#!=	Not equal	x != y	
+#>	Greater than	x > y	
+#<	Less than	x < y	
+#>=	Greater than or equal to	x >= y	
+#<=	Less than or equal to	x <= y
+
+#2
+x = 5
+y = 3
+print(x == y)
+print(x != y)
+print(x > y)
+print(x < y)
+print(x >= y)
+print(x <= y)
+
+#3
+x = 5
+print(1 < x < 10)
+print(1 < x and x < 10)
+
+#4
+a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+
+#5
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)  
---
+++ practice_2/boolean/boolean_operators.py
@@ -0,0 +1,20 @@
+#1
+x = 5
+print(x > 0 and x < 10)
+
+#2
+x = 5
+print(x < 5 or x > 10)
+
+#3
+x = 5
+print(not(x > 3 and x < 10))
+
+#4
+def myFunction() :
+  return True
+print(myFunction())
+
+#5
+x = 200
+print(isinstance(x, int))
