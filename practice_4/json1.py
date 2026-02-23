3b7a58dbc2626c045bb128de39a9f3f262866884
mirastolegen <mirastolegen52@gmail.com>
2026-02-23 22:55:39 +0500

    Add files via upload

---
+++ practice_4/json1.py
@@ -0,0 +1,21 @@
+import json
+
+
+with open("sample-data.json") as f:
+    data = json.load(f)
+
+print("Interface Status")
+print("=" * 80)
+print(f"{'DN':50} {'Description':20} {'Speed':7} {'MTU':5}")
+print("-" * 80)
+
+
+for item in data["imdata"]:
+    attrs = item["l1PhysIf"]["attributes"]
+
+    dn = attrs.get("dn", "")
+    descr = attrs.get("descr", "")
+    speed = attrs.get("speed", "")
+    mtu = attrs.get("mtu", "")
+
+    print(f"{dn:50} {descr:20} {speed:7} {mtu:5}")
